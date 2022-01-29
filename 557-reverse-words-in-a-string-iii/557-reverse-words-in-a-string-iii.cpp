de302cc61724be609c5a2a8d3281e2a1455240df
Aditi Sharma <aditiadisharma2@gmail.com>
2022-01-30 00:44:11 +0530

    Time: 16 ms (74.98%), Space: 9.6 MB (74.74%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int start = 0;
+        int start = 0; //to track start of a word
         for(int i=0;i<s.size();i++){
           if(s[i]==' '){
             reverse(s.begin()+start, s.begin()+i);
              start = i+1;
           }
         }
-        reverse(s.begin()+start, s.end());
+        reverse(s.begin()+start, s.end()); //for last word in sentence
         return s;
     }
 };
