daabd5bf437a43926afed2576017da9bd5b69b1e
Aditi Sharma <aditiadisharma2@gmail.com>
2022-02-02 21:51:04 +0530

    Time: 23 ms (15.70%), Space: 7.4 MB (46.65%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        if(s==t){
-         return true;
-        }
-        else{
+        if(s.size()!=t.size()){
             return false;
         }
+        
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return (s==t);
     }
 };
