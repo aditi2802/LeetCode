5766e6aec1ce35ead816f7126182d877dcffa6ec
Aditi Sharma <aditiadisharma2@gmail.com>
2022-02-06 22:11:15 +0530

    Time: 4 ms (86.50%), Space: 11 MB (37.19%) - LeetHub

--- 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
@@ -7,8 +7,8 @@ public:
         }
         
         int count = 1;
-        int j = 1;
-        for(int i=1;i<n;i++){
+        int j = 1;                   //We use pointer i to iterate over the array and     
+        for(int i=1;i<n;i++){        //j to iterate over the modified array.
           if(nums[i]==nums[i-1]){
              count++;
           }
