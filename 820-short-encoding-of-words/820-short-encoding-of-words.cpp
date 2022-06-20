2d2c573cbbeece0aca442a23e62eddb160335cb7
Aditi Sharma <aditiadisharma2@gmail.com>
2022-06-20 23:50:25 +0530

    Time: 243 ms (13.37%), Space: 18.5 MB (65.84%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.cpp
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int minimumLengthEncoding(vector<string>& words) {
         set<string> s1(words.begin(),words.end());
-        set<string> s2(words.begin(), words.end());
+        //set<string> s2(words.begin(), words.end());
         
         for(auto it : s1){
             //time
             for(int i=1;i<it.size();i++){
-                s2.erase(it.substr(i));  //ime me e
+                s1.erase(it.substr(i));  //ime me e
             }
         }
         
         int ans = 0;
-        for(auto it : s2){
+        for(auto it : s1){
             ans += it.size()+1;
         }
         return ans;
