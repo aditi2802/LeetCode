3a1ddf6d4470b70afde58aad74e324f186bce795
Aditi Sharma <aditiadisharma2@gmail.com>
2022-08-05 09:30:03 +0530

    Time: 7 ms (87.65%), Space: 11 MB (97.06%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void helper(int index, int target, vector<int>&ds, vector<vector<int>>&ans, vector<int>&arr){
+        if(index==arr.size()){
+            if(target==0) ans.push_back(ds);   //base case
+        return;                                //backtrack
+        } 
+        
+        //pick element
+        if(arr[index]<=target){
+            ds.push_back(arr[index]);
+            helper(index, target-arr[index], ds, ans, arr);
+            ds.pop_back();
+            // target += arr[index];
+        }
+        
+        //not pick
+        helper(index+1, target, ds, ans, arr);
+    }
+    
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        helper(0, target, ds, ans, candidates);
+        return ans;
+    }
+};
