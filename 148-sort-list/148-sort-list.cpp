261cc96d2f0c72663e6b0829c9ef3d50bee6d534
Aditi Sharma <aditiadisharma2@gmail.com>
2022-10-07 18:58:48 +0530

    Time: 501 ms (13.07%), Space: 74.2 MB (13.83%) - LeetHub

--- 148-sort-list/148-sort-list.cpp
+++ 148-sort-list/148-sort-list.cpp
@@ -10,20 +10,59 @@
  */
 class Solution {
 public:
+    ListNode* findMid(ListNode* head){
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while(fast!=NULL && fast->next!=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+    
+    ListNode* merge(ListNode* s, ListNode* e){
+        if(s==NULL) return e;
+        if(e==NULL) return s;
+        
+        ListNode* ans = new ListNode(-1);
+        ListNode* curr = ans;
+        
+        while(s!=NULL && e!=NULL){
+            if(s->val<e->val){
+                curr->next = s;
+                curr = s;
+                s = s->next;
+            }
+            else{
+                curr->next = e;
+                curr = e;
+                e = e->next;
+            }
+        }
+        while(s!=NULL){
+            curr->next = s;
+            curr = s;
+            s = s->next;
+        }
+        while(e!=NULL){
+            curr->next = e;
+            curr = e;
+            e = e->next;
+        }
+        ans = ans->next;
+        return ans;
+    }
+    
     ListNode* sortList(ListNode* head) {
-        if(head==NULL ||head->next==NULL) return head;
+        if(head==NULL || head->next==NULL) return head;
+        ListNode* mid = findMid(head);
+        ListNode* s = head;
+        ListNode* e = mid->next;
+        mid->next = NULL;
         
-        vector<int> v;
-        for(ListNode*curr=head;curr!=NULL;curr=curr->next){
-           v.push_back(curr->val);
-        }
-        sort(v.begin(), v.end());
-        reverse(v.begin(), v.end());
-        
-        for(ListNode*curr=head;curr!=NULL;curr=curr->next){
-           curr->val = v.back();
-            v.pop_back();
-        }
-        return head;
+        s = sortList(s);
+        e = sortList(e);
+        ListNode* res = merge(s, e);
+        return res;
     }
 };
