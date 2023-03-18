be11c8845c7beeaa401283c9be69dc2aa8defe87
Aditi Sharma <aditiadisharma2@gmail.com>
2023-03-19 00:25:35 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -3,6 +3,53 @@
 using namespace std;
 
 // } Driver Code Ends
+class DisjointSet{
+    vector<int> rank;
+    vector<int> size;
+    vector<int> parent;
+public:
+    DisjointSet(int n){  
+        rank.resize(n+1, 0); 
+        parent.resize(n+1);
+        size.resize(n+1, 1);
+        for(int i=0;i<=n;i++){
+            parent[i] = i; 
+        }
+    }
+    
+    int findUPar(int node){
+        if(parent[node]==node) return node; 
+        return parent[node] = findUPar(parent[node]);  
+    }
+    
+    void unionByRank(int u, int v){
+        int pu = findUPar(u);
+        int pv = findUPar(v);
+        if(pu == pv) return; 
+        
+        if(rank[pu]<rank[pv]) parent[pu] = pv;
+        else if(rank[pv]<rank[pu]) parent[pv] = pu;
+        else{
+            parent[pu] = pv;
+            rank[pv]++;
+        }
+    }
+    
+    void unionBySize(int u, int v){
+        int pu = findUPar(u);
+        int pv = findUPar(v);
+        if(pu == pv) return;
+        
+        if(size[pu]<size[pv]){
+            parent[pu] = pv;
+            size[pv] += size[pu];
+        }
+        else{
+            parent[pv] = pu;
+            size[pu] += size[pv];
+        }
+    }
+};
 class Solution
 {
 	public:
@@ -10,27 +57,31 @@ class Solution
     int spanningTree(int V, vector<vector<int>> adj[])
     {
         // code here
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int, int>>> pq;
-        vector<int> vis(V, 0);
-        int sum = 0;
-        
-        //{wt,node}
-        pq.push({0,0});
-        while(!pq.empty()){
-            auto it = pq.top();
-            pq.pop();
-            int wt = it.first;
-            int node = it.second;
-            if(vis[node]) continue;
-            vis[node] = 1;
-            sum += wt;
-            for(auto it : adj[node]){
+        //kruskal's algo
+        int mstWt = 0;
+        vector<pair<int, pair<int,int>>> edges;
+        for(int i=0;i<V;i++){
+            for(auto it : adj[i]){
+                int node = i;
                 int adjNode = it[0];
-                int edWt = it[1];
-                if(!vis[adjNode]) pq.push({edWt,adjNode});
+                int wt = it[1];
+                edges.push_back({wt, {node,adjNode}});
             }
         }
-        return sum;
+        
+        sort(edges.begin(), edges.end());
+        DisjointSet ds(V);
+        for(auto it : edges){
+            int wt = it.first;
+            int u = it.second.first;
+            int v = it.second.second;
+            
+            if(ds.findUPar(u)!=ds.findUPar(v)){
+                mstWt += wt;
+                ds.unionBySize(u,v);
+            }
+        }
+        return mstWt;
     }
 };
 
