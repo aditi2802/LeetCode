c328ab111bfafee69b4ee367827d2728fa335b90
Aditi Sharma <aditiadisharma2@gmail.com>
2023-05-22 23:28:00 +0530

    Time: 12 ms (79.51%), Space: 10.4 MB (71.03%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,5 +1,23 @@
 class Solution {
 private:
+    //Using Binary Search
+    public:
+    int lengthOfLIS(vector<int>& nums){
+        vector<int> temp;
+        temp.push_back(nums[0]);
+        
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]>temp.back()) temp.push_back(nums[i]);
+            
+            else{
+                auto ind = lower_bound(temp.begin(), temp.end(), nums[i]);
+                *ind = nums[i];
+            }
+        }
+        return temp.size();
+    }
+    
+    /* Using DP
     int f(int i, int previ, int n, vector<int>& nums, vector<vector<int>> &dp){
         if(i==n) return 0;
         if(dp[i][previ+1]!=-1) return dp[i][previ+1];
@@ -15,5 +33,5 @@ public:
         int n = nums.size();
         vector<vector<int>> dp(n, vector<int>(n+1, -1));
         return f(0, -1, n, nums, dp);
-    }
+    }*/
 };
