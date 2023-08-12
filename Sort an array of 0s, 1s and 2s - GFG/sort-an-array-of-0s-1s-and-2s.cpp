1f6ccd949e94613ad5f2cae7fb2f759b69069ca7
Aditi Sharma <aditiadisharma2@gmail.com>
2023-08-12 21:53:08 +0530

    Added solution - LeetHub

--- Sort an array of 0s, 1s and 2s - GFG/sort-an-array-of-0s-1s-and-2s.cpp
+++ Sort an array of 0s, 1s and 2s - GFG/sort-an-array-of-0s-1s-and-2s.cpp
@@ -1,40 +1,31 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 #include<bits/stdc++.h>
 using namespace std;
 
 
 
- // } Driver Code Ends
+// } Driver Code Ends
 class Solution
 {
     public:
     void sort012(int a[], int n)
     {
-        // coode here 
-        int zero = 0;
-        int one = 0;
-        int two = 0;
-        
+        // code here 
+        int zeros = 0, ones = 0, twos = 0;
         for(int i=0;i<n;i++){
-            if(a[i]==0) zero++;
-            else if(a[i]==1) one++;
-            else two++;
+            if(a[i]==0) zeros++;
+            if(a[i]==1) ones++;
+            if(a[i]==2) twos++;
         }
         
-        for(int i=0;i<zero;i++){
-            a[i] = 0;
-        }
-        for(int i=zero;i<zero+one;i++){
-            a[i] = 1;
-        }
-        for(int i=zero+one;i<zero+one+two;i++){
-            a[i] = 2;
-        }
+        for(int i=0;i<zeros;i++) a[i] = 0;
+        for(int i=zeros;i<zeros+ones;i++) a[i] = 1;
+        for(int i=zeros+ones;i<zeros+ones+twos;i++) a[i] = 2;
     }
     
 };
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 int main() {
 
     int t;
@@ -62,4 +53,5 @@ int main() {
     return 0;
 }
 
-  // } Driver Code Ends
+
+// } Driver Code Ends
