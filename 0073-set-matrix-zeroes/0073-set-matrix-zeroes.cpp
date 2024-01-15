e8e748690f612c8d1da0ee4b5c558a21dc2e4dd4
Aditi Sharma <aditiadisharma2@gmail.com>
2024-01-15 15:12:20 +0530

    Time: 8 ms (88.71%), Space: 13.8 MB (81.00%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,23 +3,31 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        
-        vector<int> r(m);
-        vector<int> c(n);
-    
+        int col0 = 1;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j] == 0){
-                    r[i] = 1;
-                    c[j] = 1;
+                    matrix[i][0] = 0;
+                    if(j!=0) matrix[0][j] = 0;
+                    else col0 = 0;
                 }
             }
         }
         
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(r[i] || c[j]) matrix[i][j] = 0;
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][j]!=0){
+                    if(matrix[0][j]==0 || matrix[i][0]==0) matrix[i][j] = 0;
+                }
             }
+        }
+        
+        if(matrix[0][0]==0){
+            for(int j=0;j<n;j++) matrix[0][j] = 0;
+        }
+        
+        if(col0==0){
+            for(int i=0;i<m;i++) matrix[i][0] = 0;
         }
     }
 };
