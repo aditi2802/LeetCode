0a4e02738c3681e438d0f700de7dcca301700984
Aditi Sharma <aditiadisharma2@gmail.com>
2024-01-22 22:46:30 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (11.87%) - LeetHub

--- unique-paths/unique-paths.cpp
+++ unique-paths/unique-paths.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
-    int f(int i, int j, vector<vector<int>> &dp){
-        if(i==0 && j==0) return 1;
+    //Recursive
+    // int helper(int i, int j, int m, int n){
+    //     if(i>=m || j>=n) return 0;
+    //     if(i==m-1 && j==n-1) return 1;
+    //     int right = helper(i, j+1, m, n);
+    //     int down = helper(i+1, j, m, n);
+    //     return right+down;
+    // }
+    
+    // int uniquePaths(int m, int n) {
+    //     return helper(0, 0, m, n);
+    // }
+    
+    //dp solution
+    int helper(int i, int j, vector<vector<int>> &dp){
         if(i<0 || j<0) return 0;
+        if(i==0 && j==0) return 1;
         if(dp[i][j]!=-1) return dp[i][j];
-        int right = f(i, j-1, dp);
-        int down = f(i-1, j, dp);
+        int right = helper(i, j-1, dp);
+        int down = helper(i-1, j, dp);
         return dp[i][j] = right+down;
-    } 
+    }
     
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return f(m-1, n-1, dp);
+        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
+        return helper(m-1, n-1, dp);
     }
 };
